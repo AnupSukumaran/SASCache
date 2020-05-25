3cb46ffb65906f1a678e9772bc84398b0b3a181d
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-05-25 19:50:14 +0530

    - New One

---
+++ Tests/Mac/Helpers/NSImage+ExtensionsTests.swift
@@ -0,0 +1,20 @@
+#if !os(iOS)
+import Cocoa
+
+
+@testable import Cache
+
+extension NSImage {
+  func isEqualToImage(_ image: NSImage) -> Bool {
+    return data == image.data
+  }
+
+  var data: Data {
+    let representation = tiffRepresentation!
+    let imageFileType: NSBitmapImageRep.FileType = .png
+
+    return NSBitmapImageRep(data: representation)!
+      .representation(using: imageFileType, properties: [:])!
+  }
+}
+#endif
